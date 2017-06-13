b526068aff83398ecca54dc13bfaa072d8fda31d
Barbour Smith <bar@makesmithtech.com>
2017-06-13 14:56:24 -0700

    All points are marked

--- UIElements/calibrationGrid.py
+++ UIElements/calibrationGrid.py
@@ -19,7 +19,9 @@ class CalibrationGrid(GridLayout):
         '''
         
         #coordinates of points to cut
-        listOfPoints = [(0,0), (100, 100), (0,100), (-100, 100)]
+        listOfPoints = [(-1080, 460),  (-540, 460), (0,  460), ( 540,  460), ( 1080,  460), 
+                        ( 1080,   0),  ( 540,   0), (0,    0), (-540,    0), (-1080,    0),
+                        (-1080, -460), (-540,-460), (0, -460), ( 540, -460), ( 1080, -460)]
         
         print "run calibration"
         self.data.units = "MM"
@@ -34,6 +36,9 @@ class CalibrationGrid(GridLayout):
         
         for point in listOfPoints:
             self.markPoint(point)
+        
+        self.data.gcode_queue.put("G0 Z5 ")
+        self.data.gcode_queue.put("G0 X0 Y0 ")
     
     def markPoint(self, point):
         '''
@@ -44,6 +49,13 @@ class CalibrationGrid(GridLayout):
         print "mark point"
         print point[0]
         print point[1]
+        
+        self.data.gcode_queue.put("G0 Z5 ")
+        moveString = "G0 X" + str(point[0]) + " Y" + str(point[1])
+        self.data.gcode_queue.put(moveString)
+        self.data.gcode_queue.put(moveString)
+        self.data.gcode_queue.put("G0 Z-6 ")
+        self.data.gcode_queue.put("G0 Z5 ")
     
     def stop(self):
         '''
